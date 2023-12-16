f5a42f33d4ac1769f8c0bac842fb20ad3e8fffe6
Shahrizan <shahrizan.learner@gmail.com>
2023-12-16 16:46:34 +0700

    on going implement playlists plugin

---
+++ src/api/playlists/handler.js
@@ -0,0 +1,19 @@
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
+  
+  async postPlaylistHandler(request, h) {
+    this._validator.validatePlaylistsPayload(request.payload);
+    
+    const { name, owner } = request.payload;
+    
+    const id = await this._service.addPlaylist({name, owner});
+    
+    
+    
+  }
+}
+
+module.exports = PlaylistsHandler;
